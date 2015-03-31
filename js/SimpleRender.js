1be9673849060c38156f986de9dabacfb99b402f
Riley Major <GitHub@RileyMajor.com>
2015-03-31 02:27:27 -0500

    Improve Grid Rendering; Add JSON box * Improved the grid rendering so that it respects all properties on all objects in the array. * Added JSON box to extract JSON from page (and allow edits to JSON).

--- js/SimpleRender.js
+++ js/SimpleRender.js
@@ -1,21 +1,32 @@
-function MakeHTMLTableFromArray(Data, TableObject, IncludeHeader)
+function MakeHTMLTableFromArray(Data, TableObject, IncludeHeader, fncHTMLEncode)
 {
 	var strTableHTML = "<tbody>";
-	if (typeof(IncludeHeader) === "boolean" ? IncludeHeader : true)
-	{
-		for (var prop in Data[0])
-		{
-			strTableHTML += "<th>" + prop + "</td>";
-		}
-	}
+	var strTableHTMLHeader = "";
+	var objHeaders = [];
+	// First, loop over the whole data array to find all possible properties.
+	// Collect them in an object which we can iterate over to build rows.
 	for (var i = 0; i < Data.length; i++)
 	{
 		strTableHTML += "<tr>";
 		for (var prop in Data[i])
 		{
-			strTableHTML += "<td>" + Data[i][prop] + "</td>";
+			objHeaders[prop] = prop;
 		}
-		strTableHTML += "<tr>"; "</tr>";
+		for (var header in objHeaders)
+		{
+			strTableHTML += "<td>" + fncHTMLEncode(Data[i][objHeaders[header]]) + "</td>";
+		}
+		strTableHTML += "</tr>";
+	}
+	if (typeof(IncludeHeader) === "boolean" ? IncludeHeader : true)
+	{
+		strTableHTMLHeader += "<thead><tr>";
+		for (var header in objHeaders)
+		{
+			strTableHTMLHeader += "<th>" + fncHTMLEncode(objHeaders[header]) + "</th>";
+		}
+		strTableHTMLHeader += "</tr></thead>";
+		strTableHTML = strTableHTMLHeader + strTableHTML;
 	}
 	strTableHTML += "</tbody>";
 	TableObject.innerHTML = strTableHTML;
